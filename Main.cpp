e88e613ad2756ec1e64bd572bf5950463a0348a7
lmontgom <lmontgom@cs.odu.edu>
2018-12-06 15:25:10 -0500

    the parsing function now splits on commas and assignes to temp values to be used to create a Node object

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include "Node.h"
 
 using namespace std;
 
@@ -20,14 +21,36 @@ int main(){
   return 0;
 }
 
+/*
+ / simple function to parse csv into data structure
+ /
+*/
 bool parse_data_file(){
   string currLine;
+  int part_counter = 3;
+  int curr_x;
+  int curr_y;
+  int curr_id;
+
   ifstream myfile("santa_cities.csv");
   if (myfile.is_open())
   {
-    while ( getline (myfile,currLine) )
+    while ( getline (myfile,currLine, ',') )
     {
-      cout << currLine << '\n';
+      if(part_counter % 3 == 0){
+        curr_id = stoi(currLine);
+        cout << "id = " << curr_id << '\n';
+      }
+      else if(part_counter % 3 == 1){
+        curr_x = stoi(currLine);
+        cout << "x = " << curr_x << '\n';
+      }
+      else if(part_counter % 3 == 2){
+        curr_y = stoi(currLine);
+        cout << "y = " << curr_y << '\n';
+      }
+      
+      part_counter++;
     }
     myfile.close();
     return true;
